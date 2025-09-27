0d98668cc17d31e88bd6e2a31212e2455e753832
Salem Mohammed Shamakh <salem.m.s2030@gmail.com>
2025-09-27 13:28:35 +0300

    Update to use expo-router

--- app/index.tsx
+++ app/index.tsx
@@ -1,34 +1,41 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View as RNView } from 'react-native';
+import { styled } from 'react-native-css';
+import '../global.css';
+import { View as ExpoView } from 'react-native-css/components';
+
+const View = styled(RNView, {
+  className: { target: 'style' },
+});
 
 export default function Page() {
   return (
-    <View style={styles.container}>
-      <View style={styles.main}>
+    <ExpoView style={styles.container} className="bg-red-200">
+      <View style={styles.main} className="bg-red-300">
         <Text style={styles.title}>Hello World</Text>
         <Text style={styles.subtitle}>This is the first page of your app.</Text>
       </View>
-    </View>
+    </ExpoView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
+    alignItems: 'center',
     padding: 24,
   },
   main: {
     flex: 1,
-    justifyContent: "center",
+    justifyContent: 'center',
     maxWidth: 960,
-    marginHorizontal: "auto",
+    marginHorizontal: 'auto',
   },
   title: {
     fontSize: 64,
-    fontWeight: "bold",
+    fontWeight: 'bold',
   },
   subtitle: {
     fontSize: 36,
-    color: "#38434D",
+    color: '#38434D',
   },
 });
